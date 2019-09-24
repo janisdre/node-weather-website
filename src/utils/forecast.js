24932ce5d099dcdfe5530dfbcc3aed5415683644
Janis <jdregeris@gmail.com>
2019-09-24 18:03:32 +0300

    Add geocode info

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/35056bba6d9d8b776bb3624a87eda12d/' + latitude + ',' + longitude
+    const url = 'https://api.darksky.net/forecast/35056bba6d9d8b776bb3624a87eda12d/' + latitude + ',' + longitude + '?units=si&lang=lv'
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. The high is ' + body.daily.data[0].temperatureHigh + '. The low is ' + body.daily.data[0].temperatureLow + ' There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
